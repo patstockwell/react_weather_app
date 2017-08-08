93251fd3e1a3e023b8bd066bcb24fb863455f7e0
Patrick Stockwell <stockwellpatrick@gmail.com>
2017-08-08 17:06:55 +1000

    add bootstrap to rendered html, store form input with event handler, call fetchData() from store

--- src/pages/ForecastPage.js
+++ src/pages/ForecastPage.js
@@ -7,6 +7,7 @@ class Forecast extends Component {
     constructor(props) {
         super(props)
         this.state = {
+            query: '',
             forecastData: [
                 { maxTemp: 32, id: 123049870 },
                 { maxTemp: 33, id: 238475619 },
@@ -14,10 +15,19 @@ class Forecast extends Component {
                 { maxTemp: 25, id: 585875857 }
             ]
         }
+        this.handleInput = this.handleInput.bind(this)
+        this.fetchNewData = this.fetchNewData.bind(this)
     }
 
-    fetchNewData() {
-        ForecastActions.fetchData('Melbourne')
+    handleInput(event) {
+        this.setState({
+            query: event.target.value
+        })
+    }
+
+    fetchNewData(event) {
+        event.preventDefault()
+        ForecastActions.fetchData(this.state.query)
     }
 
     componentWillMount() {
@@ -31,11 +41,22 @@ class Forecast extends Component {
             return <DailyForecastItem key={forecast.id} data={forecast}/>
         })
         return(
-            <div>
-                Forecast page
-
-                <div>{dailyForecasts}</div>
-                <button onClick={this.fetchNewData}>Fetch New Data</button>
+            <div className="container">
+                <div className="row">
+                    Forecast page
+                </div>
+                <div className="row">
+                    {dailyForecasts}
+                </div>
+                <div className="row my-5">
+                    <form onSubmit={this.fetchNewData}>
+                        <div className="form-group">
+                            <input value={this.state.query} onInput={this.handleInput} type="text" className="form-control my-3" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="City" />
+                            <small id="emailHelp" className="form-text text-muted my-1">Get the 5 day forecast for any city in Australia.</small>
+                            <button type="submit" className="btn btn-primary my-3">Fetch New Data</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         )
     }
