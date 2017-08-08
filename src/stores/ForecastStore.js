577241d07ac55fd240e46cf66813e01ad2de465b
Patrick Stockwell <stockwellpatrick@gmail.com>
2017-08-08 17:05:42 +1000

    add openweathermap api key, import city list, implement getCityId(), add axios.get()

--- src/stores/ForecastStore.js
+++ src/stores/ForecastStore.js
@@ -1,20 +1,58 @@
 import { EventEmitter } from 'events'
-
+import axios from 'axios'
 import Dispatcher from '../dispatcher/Dispatcher'
+import cityList from '../australian.city.list.min'
 
 class ForecastStore extends EventEmitter {
     constructor() {
         super()
         this.forecastData = [
-        
+
         ]
         // bind the functions IN this class TO this class
         this.handleChange = this.handleChange.bind(this)
         this.fetchApiData = this.fetchApiData.bind(this)
     }
 
+    getCityId(name) {
+        if(name) {
+            // loop through the city lsit
+            for(let x = 0; x < cityList.length; x++) {
+                // if the name is found, return it's ID
+                if(name.toLowerCase() === cityList[x].name.toLowerCase()) {
+                    return cityList[x].id
+                }
+            }
+        }
+        return undefined
+    }
+
+    toCelcius(kelvin) {
+        return kelvin - 273.15
+    }
+
     fetchApiData(keyword) {
-        // go get data from API with axios
+        const id = this.getCityId(keyword)
+        console.log(id)
+        const API_KEY = 'cdfee189f0f29adbbe63a56b6140263c'
+        if(id) {
+            console.log('fetching data...')
+            // go get data from API with axios
+            axios.get('http://api.openweathermap.org/data/2.5/forecast', {
+                params: {
+                    id: id,
+                    APPID: API_KEY
+                }
+            })
+            .then(function (response) {
+                console.log(response)
+            })
+            .catch(function (error) {
+                console.log(error)
+            })
+        }
+
+
         this.forecastData = [
             { maxTemp: 13, id: 123049870 },
             { maxTemp: 12, id: 238475619 },
