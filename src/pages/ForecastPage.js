45db401b7a347579c2cdcd97fe1be11346ac3153
Patrick Stockwell <stockwellpatrick@gmail.com>
2017-08-09 21:54:50 +1000

    add this.state.lastUnfoundSearch, add img tags based on search results

--- src/pages/ForecastPage.js
+++ src/pages/ForecastPage.js
@@ -8,34 +8,48 @@ class Forecast extends Component {
         super(props)
         this.state = {
             query: '',
+            lastUnfoundSearch: null,
             forecastData: null
         }
     }
 
     componentWillMount() {
         ForecastStore.on('change', () => {
-            this.setState({forecastData: ForecastStore.getForecastData()})
+            this.setState({
+                lastUnfoundSearch: null,
+                forecastData: ForecastStore.getForecastData()
+            })
+        })
+        ForecastStore.on('error', () => {
+            this.setState({
+                lastUnfoundSearch: ForecastStore.getKeywordSearched()
+            })
         })
     }
 
     render() {
-        // const dailyForecasts = this.state.forecastData.map(forecast => {
-        //     return <DailyForecastItem key={forecast.id} data={forecast}/>
-        // })
+        let dailyForecasts
+        if(this.state.forecastData) {
+            dailyForecasts = this.state.forecastData.list.map(forecast => {
+                return <DailyForecastItem key={forecast.dt} data={forecast}/>
+            })
+        }
+        const error = this.state.lastUnfoundSearch ? `Couldn\'t find ${this.state.lastUnfoundSearch}` : ''
         const heading = this.state.forecastData ? this.state.forecastData.city.name : 'Australian weather forecast'
         console.log(this.state.forecastData)
         return(
             <div className="container">
                 <div className="forecast-heading">
-                    <h1 className="display-4 my-5">{heading}</h1>
+                    <h1 className="display-4 my-2">{heading}</h1>
                 </div>
-                <div className="row">
-                    <DailyForecastItem key="1234" data={this.state.forecastData}/>
-                    <DailyForecastItem key="2345" data={this.state.forecastData}/>
-                    <DailyForecastItem key="1223534" data={this.state.forecastData}/>
-                </div>
-                <div className="search-area my-5">
+                <div className="search-area my-2">
                     <SearchBar />
+                    <p className="error">{error}</p>
+                </div>
+                <div className="forecast-tile-area pb-5">
+                    <div className="row">
+                        { dailyForecasts }
+                    </div>
                 </div>
             </div>
         )
